6bf5f1e5bdfb7f4ab73867357ee57cdf32a8f9aa
JOHN KEYS CLOUD <90482169+JohnKeysCloud@users.noreply.github.com>
2024-01-27 17:19:23 -0500

    remove unnecessary event listeners on due date inputs

--- src/js/modifiers/modify-due-date-input.js
+++ src/js/modifiers/modify-due-date-input.js
@@ -13,13 +13,9 @@ function modifyDueDateInput(dueDateInput, formState) {
   if (formState === 'visible') {
     dueDateInput.addEventListener('blur', toggleDueDateInputType);
     dueDateInput.addEventListener('focus', toggleDueDateInputType);
-    dueDateInput.addEventListener('mousedown', toggleDueDateInputType);
-    dueDateInput.addEventListener('touchstart', toggleDueDateInputType);
   } else if (formState === 'closing') {
     dueDateInput.removeEventListener('blur', toggleDueDateInputType);
     dueDateInput.removeEventListener('focus', toggleDueDateInputType);
-    dueDateInput.removeEventListener('mousedown', toggleDueDateInputType);
-    dueDateInput.removeEventListener('touchstart', toggleDueDateInputType);
   }
 }
 
