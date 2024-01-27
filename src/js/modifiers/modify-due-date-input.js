77c44270359384e670be7bb945841410b50e174c
JOHN KEYS CLOUD <90482169+JohnKeysCloud@users.noreply.github.com>
2024-01-27 16:54:06 -0500

    modify date input event triggers to fix bug where input click is required twice on mobile to open datepicker

--- src/js/modifiers/modify-due-date-input.js
+++ src/js/modifiers/modify-due-date-input.js
@@ -10,11 +10,15 @@ function toggleDueDateInputType(e) {
 
 function modifyDueDateInput(dueDateInput, formState) {
   if (formState === 'visible') {
-    dueDateInput.addEventListener('focus', toggleDueDateInputType);
     dueDateInput.addEventListener('blur', toggleDueDateInputType);
+    dueDateInput.addEventListener('focus', toggleDueDateInputType);
+    dueDateInput.addEventListener('mousedown', toggleDueDateInputType);
+    dueDateInput.addEventListener('touchstart', toggleDueDateInputType);
   } else if (formState === 'closing') {
-    dueDateInput.removeEventListener('focus', toggleDueDateInputType);
     dueDateInput.removeEventListener('blur', toggleDueDateInputType);
+    dueDateInput.removeEventListener('focus', toggleDueDateInputType);
+    dueDateInput.removeEventListener('mousedown', toggleDueDateInputType);
+    dueDateInput.removeEventListener('touchstart', toggleDueDateInputType);
   }
 }
 
